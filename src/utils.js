179d0427cf1ad858ccc4d7ec821f0373e481bd59
Daniel Hassouni <dhassouni@gmail.com>
2017-01-19 11:20:14 -0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,45 @@
-import { isClient } from './platform'
+'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.clientOnly = exports.noop = exports.equalRecords = exports.find = exports.arrayify = undefined;
 
+var _platform = require('./platform');
 
-const arrayify = (x) => (
-  Array.isArray(x) ? x : [x]
-)
+var arrayify = function arrayify(x) {
+  return Array.isArray(x) ? x : [x];
+};
 
-const find = (f, xs) => (
-  xs.reduce(((b, x) => b ? b : f(x) ? x : null), null)
-)
+var find = function find(f, xs) {
+  return xs.reduce(function (b, x) {
+    return b ? b : f(x) ? x : null;
+  }, null);
+};
 
-const equalRecords = (o1, o2) => {
-  for (const key in o1) if (o1[key] !== o2[key]) return false
-  return true
-}
+var equalRecords = function equalRecords(o1, o2) {
+  for (var key in o1) {
+    if (o1[key] !== o2[key]) return false;
+  }return true;
+};
 
-const noop = () => (
-  undefined
-)
+var noop = function noop() {
+  return undefined;
+};
 
-const clientOnly = (f) => (
-  isClient ? f : noop
-)
+var clientOnly = function clientOnly(f) {
+  return _platform.isClient ? f : noop;
+};
 
-
-
-export default {
-  arrayify,
-  find,
-  equalRecords,
-  noop,
-  clientOnly,
-}
-export {
-  arrayify,
-  find,
-  equalRecords,
-  noop,
-  clientOnly,
-}
+exports.default = {
+  arrayify: arrayify,
+  find: find,
+  equalRecords: equalRecords,
+  noop: noop,
+  clientOnly: clientOnly
+};
+exports.arrayify = arrayify;
+exports.find = find;
+exports.equalRecords = equalRecords;
+exports.noop = noop;
+exports.clientOnly = clientOnly;
