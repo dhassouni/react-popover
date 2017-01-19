dfac5ec1ff88eaf9d95aaae9b65a83319805c65f
Daniel Hassouni <dhassouni@gmail.com>
2017-01-19 11:45:40 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -517,6 +517,4 @@ var Popover = (0, _react.createClass)({
   }
 });
 
-// Support for CJS
-// http://stackoverflow.com/questions/33505992/babel-6-changes-how-it-exports-default
 module.exports = Popover;
